bb3c923dbd8587133d201d7ad28ffa136f523a21
“Tharun <krtkalugalla@gmail.com>
2026-03-11 16:57:05 +0530

    initial commit

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 from pathlib import Path
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql+psycopg2://postgres:password@localhost:5432/brain_tumor"
+    DATABASE_URL: str = "postgresql+psycopg2://postgres:admin@localhost:5432/brain_tumor"
     SECRET_KEY: str = "CHANGE_ME"              # use a strong random string
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
